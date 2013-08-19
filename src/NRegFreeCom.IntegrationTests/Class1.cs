6ee1095afe88a072e198ca592488a1f092f4b49a
Dzmitry Lahoda <Dzmitry.Lahoda@gmail.com>
2013-08-19 16:01:40 +0300

    added test projects

---
+++ src/NRegFreeCom.IntegrationTests/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NRegFreeCom.IntegrationTests
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/RegFreeComResources/RegFreeComResources.cpp
@@ -0,0 +1,6 @@
+// RegFreeComResources.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+
+
